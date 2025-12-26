a82136989b2d9c9cb72d42d4221c9f86658e033a
vishalvermacore <vishalvermaa.50103@gmail.com>
2025-12-26 22:31:40 +0530

    update resume and tags

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -11,6 +11,24 @@ const projects = [
     github:
       "https://github.com/vishalvermacore/EchoStream-Video-Calling-Realtime-Chat-Social-App",
   },
+  {
+    title: "Tredzo – Zerodha-Style Stock Trading Web Application",
+    description:
+      "A MERN stack stock trading web application inspired by Zerodha, featuring a realistic interface with interactive modules for watchlists, order execution, and portfolio analytics.",
+    image: "/projects/project2.jpeg",
+    tags: [
+      "React.js",
+      "Material UI",
+      "Bootstrap",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "Chart.js",
+      "Jest",
+    ],
+    link: "https://github.com/vishalvermacore/Stock-Trade-Application",
+    github: "https://github.com/vishalvermacore/Stock-Trade-Application",
+  },
 ];
 
 export const Projects = () => {
@@ -107,11 +125,14 @@ export const Projects = () => {
         </div>
 
         {/* View All CTA */}
+
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
-            View All Projects
-            <ArrowUpRight className="w-5 h-5" />
-          </AnimatedBorderButton>
+          <a href="https://github.com/vishalvermacore">
+            <AnimatedBorderButton>
+              View All Projects
+              <ArrowUpRight className="w-5 h-5" />
+            </AnimatedBorderButton>
+          </a>
         </div>
       </div>
     </section>
